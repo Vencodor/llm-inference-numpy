c98addff38e483372da0aed203397bf1097d7924
김두성 <1324qqww1234@gmail.com>
2026-06-19 21:49:45 +0900

    feat: add KV-cache to attention

--- infernce_kvcache.py
+++ infernce_kvcache.py
@@ -6,12 +6,12 @@ def softmax(x, axis=-1) -> np.ndarray:
     e = np.exp(x - x.max(axis=axis, keepdims=True))
     return e / e.sum(axis=axis, keepdims=True)
 
-def embedding(w, ids):
+def embedding(w, ids, index=0):
     wte = w['wte']
     wpe = w['wpe']
 
     token_embed = wte[ids]
-    pos_embed = wpe[np.arange(len(ids))]
+    pos_embed = wpe[np.arange(len(ids))+index]
 
     return token_embed + pos_embed
 
@@ -23,33 +23,52 @@ def layernorm(x: np.ndarray, g, b):
 
     return x * g + b
 
-def attention(x: np.ndarray, attn_w, attn_b, proj_w, proj_b):
-    qkv = x @ attn_w + attn_b #(len, 768*3)
-    q,k,v = np.split(qkv, 3, axis=1) #(len, 768)
+def spliter(x: np.ndarray, len1, dims, head=12):
+    x = x.reshape(len1, head, dims) #(len, 12, 64)
+    return x.transpose(1, 0, 2) #(12, len, 64)
 
-    head = 12
+def attention(x: np.ndarray, index, attn_w, attn_b, proj_w, proj_b):
     len1 = x.shape[0]
     len2 = x.shape[1]
-    dims = len2 // head
+    dims = len2 // 12
 
-    def spliter(x: np.ndarray):
-        x = x.reshape(len1, head, dims) #(len, 12, 64)
-        return x.transpose(1, 0, 2) #(12, len, 64)
-    
-    q = spliter(q)
-    k = spliter(k)
-    v = spliter(v)
+    if kv_cache[index] is None: #prefill, x=(len,728)
+        qkv = x @ attn_w + attn_b
+        q,k,v = np.split(qkv, 3, axis=1)
+        
+        q = spliter(q, len1, dims)
+        k = spliter(k, len1, dims)
+        v = spliter(v, len1, dims)
 
-    scores = q @ k.transpose(0, 2, 1) #(12, len, 64) @ (12, 64, len) = (12, len)
-    scores = scores / np.sqrt(64)
+        kv_cache[index] = k,v
 
-    mask = np.triu(np.ones((len1, len1)), k=1)
-    scores = scores + mask * (-1e9)
+        scores = q @ k.transpose(0, 2, 1)
+        scores = scores / np.sqrt(64)
+
+        mask = np.triu(np.ones((len1, len1)), k=1) * (-1e9)
+        scores = scores + mask
+    else: #decode, x=(1,728)
+        k,v = kv_cache[index] #(len-1, 728)
+
+        qkv1 = x @ attn_w + attn_b
+        q,k1,v1 = np.split(qkv1, 3, axis=1)
+
+        q = spliter(q, len1, dims)
+        k1 = spliter(k1, len1, dims)
+        v1 = spliter(v1, len1, dims)
+        
+        k = np.concatenate([k, k1], axis=1)
+        v = np.concatenate([v, v1], axis=1)
+
+        kv_cache[index] = k, v
+
+        scores = q @ k.transpose(0, 2, 1)
+        scores = scores / np.sqrt(64)
 
     scores = softmax(scores)
 
-    out = scores @ v #(12, len, 64)
-    out = out.transpose(1, 0, 2) #(len, 12, 64)
+    out = scores @ v
+    out = out.transpose(1, 0, 2)
     out = out.reshape(len1, len2)
 
     return out @ proj_w + proj_b
@@ -67,6 +86,7 @@ def mlp(x, fc_w, fc_b, proj_w, proj_b):
 def block(w, i, x):
     x = x + attention(
         layernorm(x, w[f"b{i}.ln1_g"], w[f"b{i}.ln1_b"]),
+        i,
         w[f"b{i}.attn_w"], w[f"b{i}.attn_b"],
         w[f"b{i}.attn_proj_w"], w[f"b{i}.attn_proj_b"],
     )
@@ -79,11 +99,11 @@ def block(w, i, x):
 
     return x
 
-def gpt2_forward(w, ids) -> np.ndarray:
-    x = embedding(w, ids)
+def gpt2_forward(w, ids, i) -> np.ndarray:
+    x = embedding(w, ids, i)
 
     for i in range(12):
-        x = block(w,i, x)
+        x = block(w,i , x)
     
     x = layernorm(x, W['lnf_g'], W['lnf_b']) #(len, 768)
 
@@ -91,23 +111,28 @@ def gpt2_forward(w, ids) -> np.ndarray:
 
 def generate(prompt,token = 10, temperature = 1.0, top_k = 5):
     ids = enc.encode(prompt)
-
+    
+    prefilled = False
     for _ in range(token):
-        logit = gpt2_forward(W, ids)[-1]
+        if not prefilled:
+            logit = gpt2_forward(W, ids, 0)[-1]
+        else:
+            logit = gpt2_forward(W, [ids[-1]], len(ids)-1)[-1]
         
         probs = softmax(logit / temperature)
         top_ids = np.argsort(probs)[-top_k:]
         top_probs = probs[top_ids]
         top_probs = top_probs / sum(top_probs)
-
         next = np.random.choice(top_ids, p=top_probs)
+
         ids.append(int(next))
+        prefilled = True
 
     return enc.decode(ids)
 
 enc = tiktoken.get_encoding('gpt2')
 W = np.load("gpt2_all.npz")
+kv_cache: list[np.ndarray] = [None for _ in range(12)]
 np.random.seed(0)
-
 
 print(generate('hello my name is '))
